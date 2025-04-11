ac52d681fa36887aeba4489f9aa3bc10045e1e90
Shivani Gupta <shivaniguptaa2@gmail.com>
2025-04-11 07:11:41 +0530

    Added brand_generator

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python_DS
 
 This reporsitory contains all the learning from Python.
+Day 1 : Small project of Brand Generator
---
+++ brand_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to Brand Generator Program")
+city = input("Enter your city name")
+print(city)
+name = input("Enter your pet name")
+print(name)
+print('Brand for you is :', name+city)
