dbab5b3ef4923039c8e30514d978e8a73441b0a4
Shivani Gupta <shivaniguptaa2@gmail.com>
2025-05-16 08:32:30 +0530

    implmented OPPS also added direction to the snake movement

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,21 +1,28 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(600,600)
 screen.bgcolor('Black')
 screen.title('My Snake Game')
+"""tracer function is to stop the animation"""
+screen.tracer(0)
+is_game_on = True
 
-square_positions = [(0,0),(-20,0),(-40,0)]
-for positions in square_positions:
-    segment = Turtle('square')
-    segment.color('white')
-    segment.goto(positions)
+snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
-
-
-
-
+while is_game_on:
+    screen.update() 
+    time.sleep(0.1)
+        #this update function will show animation we are calling it here so once all of them are created then we can see
+    snake.move_snake()
 
 
 
