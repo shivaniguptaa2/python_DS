736ee9c4bb0096b0f432c648d507efce169507dd
Shivani Gupta <shivaniguptaa2@gmail.com>
2025-06-01 22:05:54 +0530

    handling csv data

--- CSV/csv.py
+++
@@ -1,5 +0,0 @@
-# with open("CSV/weather_data.csv") as file:
-#     rows = file.readlines()
-#     print(rows)
-
-"""We can use this to read the data from CSV """
---
+++ CSV/main.py
@@ -0,0 +1,24 @@
+# with open("CSV/weather_data.csv") as file:
+#     rows = file.readlines()
+#     print(rows)
+
+"""We can use this to read the data from CSV """
+
+# import csv
+
+# with open(r"e:\python_DS\CSV\weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperature = []
+#     for row in data:
+#         temperature.append(row[1])
+#         # print(row)
+#     print(temperature)
+
+"""Here it is good number of lines, but pandas can be used very efficiently to handle csv data"""
+
+import pandas
+
+data = pandas.read_csv("CSV/weather_data.csv")
+print(data["temp"])
+
+"""The number of lines reduced plus the formatting as table is wonderful"""
