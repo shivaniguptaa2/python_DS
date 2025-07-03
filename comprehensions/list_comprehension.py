d0e832f88ea16057a95532534bac7fec6c7f54d9
Shivani Gupta <shivaniguptaa2@gmail.com>
2025-07-03 07:42:30 +0530

    Revisited List Comprehension

--- comprehensions/list_comprehension.py
+++ comprehensions/list_comprehension.py
@@ -9,15 +9,24 @@
  """
 
 number = [1,2,3]
-# new_list = [new_item for item in list1]
-new_list = [n+1 for n in number]
-print(new_list)
+# new_num =[]
+# for num in number:
+#     new_num.append(num+1)
+# print(new_num)
+"""For creating this list, we iterated through exixting list added some conditions over that to get value"""
+
+# new_list = [n+1 for n in number]
+# print(new_list)
+
+"""Syntax for list comprehension new_list = [new_item for item in list1]"""
+"""Using List comprehension number of lines are redeuced significantly"""
+
 name = 'Shivani'
 name_list = [letter for letter in name]
 print(name_list)
-num_list = [2*num for num in range(1,5)]
-print(num_list)
+# num_list = [2*num for num in range(1,5)]
+# print(num_list)
 
-names = ['Shiv','Bhanjo','Shivani','Khushi','Rama','laxman']
-new_name = [name.upper() for name in names if len(name)>5]
-print(new_name)
+# names = ['Shiv','Bhanjo','Shivani','Khushi','Rama','laxman']
+# new_name = [name.upper() for name in names if len(name)>5]
+# print(new_name)
